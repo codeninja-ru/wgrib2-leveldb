45226c1a9aea64b5ecc99d7ec6e360a6cb21db99
codeninja-ru <aesuaS8vooKahog0>
2014-07-07 14:50:33 +1100

    downoaded wgrib2 v2.0.0

---
+++ wgrib2/config.h
@@ -0,0 +1,16 @@
+/* config.h */
+#define USE_REGEX
+#define USE_TIGGE
+#define USE_G2CLIB
+//#define USE_PROJ4
+#define USE_JASPER
+#define USE_NETCDF3
+//#define USE_NETCDF4
+//#define USE_MYSQL
+//#define USE_IPOLATES
+//#define USE_UDF
+#define CC "cc"
+#define FORTRAN "f77"
+#define BUILD_COMMENTS "stock build"
+#define USE_PNG
+//#define WMO_VALIDATION
